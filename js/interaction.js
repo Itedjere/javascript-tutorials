efd7aa290f99c07c7fd568d69da28b648a3e58b2
Godspower Itedjere <itedjereg77@gmail.com>
2023-07-04 21:14:16 +0100

    I just git initialized this folder

---
+++ js/interaction.js
@@ -0,0 +1,9 @@
+// alert("This is a Modal");
+
+// let yourName = prompt("What is your name");
+
+// alert(`Nice To See You ${yourName}`);
+
+let isMale = confirm("Are You A Male");
+
+alert( isMale );
